b42d25bc68e80b0efbf176efade6f07668730432
Azat <kushkarov.azat@gmail.com>
2025-03-11 21:16:15 +0300

    first commit

---
+++ src/main/java/ru/azat/TaskPlannerApiGateway/TaskPlannerApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package ru.azat.TaskPlannerApiGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskPlannerApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskPlannerApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/azat/TaskPlannerApiGateway/TaskPlannerApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.azat.TaskPlannerApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskPlannerApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
