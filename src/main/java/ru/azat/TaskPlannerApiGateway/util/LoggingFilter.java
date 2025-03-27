e57622cea91e3fd36501b9fd5cd02e74bf89e62e
Azat <kushkarov.azat@gmail.com>
2025-03-27 21:22:06 +0300

    add logging filter

---
+++ src/main/java/ru/azat/TaskPlannerApiGateway/util/LoggingFilter.java
@@ -0,0 +1,23 @@
+package ru.azat.TaskPlannerApiGateway.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class LoggingFilter implements GlobalFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        log.info("Запрос через Gateway: {} {}", request.getMethod(), request.getURI());
+        return chain.filter(exchange);
+    }
+}
+
+
